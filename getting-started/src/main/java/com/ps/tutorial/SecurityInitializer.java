eaf3867a9488ec6a95f3a77ba2b5e8486f59789a
pashashiz <pogrebnij@gmail.com>
2015-05-12 00:11:58 +0300

    changed secutiry initializer

--- getting-started/src/main/java/com/ps/tutorial/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.ps.tutorial;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
